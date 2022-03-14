53cf491b13f421d47bf37b5567ecce022db6b900
Marty <marty@kingkit101.com>
2022-03-14 21:02:22 +0800

    update cmd

--- ota/cmd.py
+++ ota/cmd.py
@@ -1,13 +1,14 @@
 import os,machine
 from utils import *
 from webduino.board import Board
+from webduino.config import Config
 
-try:
-    os.remove('cmd.py')
-except:
-    pass
+deviceId = 'marty'
+Config.load()
+Config.data['devId'] = deviceId
+Config.save()
+Board()
 
-Board(devId="ota")
 url = 'https://marty5499.github.io/pythonCode/'
 Utils.save(url+'lib/urequests.py','lib/urequests.py')
 Utils.save(url+'lib/umqtt/simple.py','lib/umqtt/simple.py')
@@ -21,4 +22,17 @@ Utils.save(url+'lib/webduino/wifi.py','lib/webduino/wifi.py')
 Utils.save(url+'lib/webduino/webserver.py','lib/webduino/webserver.py')
 Utils.save(url+'lib/webduino/debug.py','lib/webduino/debug.py')
 Utils.save(url+'lib/utils.py','lib/utils.py')
+Utils.save(url+'app/CamApp.py','main.py')
 Utils.save(url,'index.html')
+
+try:
+    os.remove('cmd.py')
+    print('remove cmd.py')
+except:
+    pass
+
+try:
+    os.remove('main.py')
+    print('remove main.py')
+except:
+    pass
