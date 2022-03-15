a54dd2096333209b8a898e0b599a8e716511ccb8
Marty <marty@kingkit101.com>
2022-03-15 10:50:39 +0800

    fix wifi connect

--- ota/init.py
+++ ota/init.py
@@ -1,8 +1,24 @@
-board_devSSID = ''
-board_device_id = ''
+board_devSSID ='wa001'
+board_device_id = 'wa001'
 
-from webduino.board import Board
 import os, usocket, time, ubinascii, network, machine
+
+def do_connect():
+    global connected
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    print('connecting to network...')
+    #sta_if.disconnect()
+    if(not sta_if.isconnected()):
+        sta_if.connect('KingKit_2.4G', 'webduino')
+    cnt = 0
+    while not sta_if.isconnected():
+        cnt = cnt + 1
+        time.sleep(0.5)
+        if cnt == 60:
+            break
+    connected = sta_if.isconnected()
+    print('network config:', sta_if.ifconfig())
 
 class Response:
 
@@ -215,7 +231,7 @@ def install(deviceId=''):
 
     # WiFi Connect
     print("connect...")
-    Board()
+    do_connect()
     print("get files...")
 
     # 開源必備
